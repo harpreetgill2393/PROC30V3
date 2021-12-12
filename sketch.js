a01b9f174682af277312604132427283272c4b7f
harpreetgill2393 <77834237+harpreetgill2393@users.noreply.github.com>
2021-12-12 18:38:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,13 +21,13 @@ var backgroundImage;
 var stones = [];
 
 function preload() {
-  zombie1 = loadImage("./assets/zombie1.png");
-  zombie2 = loadImage("./assets/zombie2.png");
+  zombie1 = loadImage("zombie1.png");
+  zombie2 = loadImage("zombie2.png");
 
-  zombie3 = loadImage("./assets/zombie3.png");
-  zombie4 = loadImage("./assets/zombie4.png");
+  zombie3 = loadImage("zombie3.png");
+  zombie4 = loadImage("zombie4.png");
 
-  backgroundImage = loadImage("./assets/background.png");
+  backgroundImage = loadImage("background.png");
 }
 
 function setup() {
